37a5d0b2ea5753a25368a59287f819e50195e62e
Joelle Hanna <joelle.hanna@epfl.ch>
2019-03-28 13:52:51 +0100

    tt

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def train():
 
     print(" ** fixed learning rate: %f (for init G)" % lr_init)
 
-    train_vid_list = train_vid_list[0:12] #5000
+    train_vid_list = train_vid_list[0:100] #5000
     #train_lr_vid_list = train_lr_vid_list[0:12] #5000
     for epoch in range(0, n_epoch_init + 1):
 
