d1e769bc433290b9e93a2d64b8ef18adbac04600
Joelle Hanna <joelle.hanna@epfl.ch>
2019-03-28 10:22:53 +0100

    main

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def train():
 
     # Evaluate discrimator on real triplets
     net_d, logits_real = SRGAN_d(t_target_image, is_train=True, reuse=False)
-    _, logits_fake = SRGAN_d(net_g, is_train=True, reuse=True) #net_g.outputs
+    _, logits_fake = SRGAN_d(net_g.outputs, is_train=True, reuse=True) #net_g.outputs
 
     #maybe add vgg (?)
     net_g_test = SRGAN_g(t_image, is_train=False, reuse=True)
@@ -70,7 +70,7 @@ def train():
     d_loss2 = tl.cost.sigmoid_cross_entropy(logits_fake, tf.zeros_like(logits_fake), name='d2')
 
     g_gan_loss = 1e-3 * tl.cost.sigmoid_cross_entropy(logits_fake, tf.ones_like(logits_fake), name='g')
-    mse_loss = tl.cost.mean_squared_error(net_g, t_target_image, is_mean=True) #net_g.outputs
+    mse_loss = tl.cost.mean_squared_error(net_g.outputs, t_target_image, is_mean=True) #net_g.outputs
     #vgg_loss = 2e-6 * tl.cost.mean_squared_error(vgg_predict_emb.outputs, vgg_target_emb.outputs, is_mean=True)
 
     # maybe add vgg_loss, ms_ssim_loss, and clipping_loss (?)
