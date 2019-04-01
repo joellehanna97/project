0e3fe138193fa4b6de998834e4bd7cacbaff1847
Joelle Hanna <joelle.hanna@epfl.ch>
2019-04-01 06:54:04 +0200

    test

--- main.py
+++ main.py
@@ -63,9 +63,9 @@ def train():
 
     # Evaluate discrimator on real triplets
     net_d, logits_real = SRGAN_d(t_target_image, is_train=True, reuse=False)
-    _, logits_fake = SRGAN_d(net_g.outputs, is_train=True, reuse=True) #net_g.outputs
+    _, logits_fake = SRGAN_d(net_g.outputs, is_train=True, reuse=True)
 
-    #maybe add vgg (?)
+
 
     ## vgg inference. 0, 1, 2, 3 BILINEAR NEAREST BICUBIC AREA
     t_target_image_224 = tf.image.resize_images(
@@ -129,8 +129,9 @@ def train():
     sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
     tl.layers.initialize_global_variables(sess)
 
-    tl.files.load_and_assign_npz(sess=sess, name=checkpoint_input_g, network=net_g)
+    tl.files.load_and_assign_npz(sess=sess, name= name=checkpoint_dir + '/g_{}_init.npz'.format(tl.global_flag['mode']), network=net_g)
     tl.files.load_and_assign_npz(sess=sess, name=checkpoint_input_d, network=net_d)
+    #tl.files.load_and_assign_npz(sess=sess, name= name=checkpoint_dir + '/g_{}_init.npz'.format(tl.global_flag['mode']), network=net_d)
     ###============================= LOAD VGG ===============================###
     vgg19_npy_path = "vgg19.npy"
     if not os.path.isfile(vgg19_npy_path):
@@ -181,9 +182,6 @@ def train():
     train_vid_imgs = tl.prepro.threading_data(train_vid_imgs, fn = crop_sub_imgs_fn,is_random=False) #fn=tl.prepro.crop, wrg=82, hrg=82, is_random=False)
     train_target_vid_imgs = tl.prepro.threading_data(train_target_vid_imgs, fn = crop_sub_imgs_fn,is_random=False)#fn=tl.prepro.crop, wrg=82, hrg=82, is_random=False) #328 328
     #train_vid_seqs = [np.concatenate([train_vid_imgs[0], train_vid_imgs[1]],2)]
-    #print(type(np.asarray(train_vid_seqs)))
-    #print(np.shape(train_vid_seqs))
-
 
     train_vid_seqs = np.stack([np.concatenate([train_vid_imgs[0], train_vid_imgs[1]], 2),
 			np.concatenate([train_vid_imgs[2], train_vid_imgs[3]], 2),
@@ -192,11 +190,8 @@ def train():
 
     tl.vis.save_images(train_target_vid_imgs, [ni, ni], save_dir_ginit + '/_train_sample_384.png')
     #tl.vis.save_images(np.asarray(train_vid_seqs), [ni, ni], save_dir_ginit + '/_train_sample_96_1.png')
-    print(np.shape(train_vid_seqs[:,:,:,0:3]))
-    print(np.shape(train_vid_seqs))
     tl.vis.save_images(train_vid_seqs[:,:,:,0:3], [ni, ni], save_dir_ginit + '/_train_sample_96_1.png')
     tl.vis.save_images(train_vid_seqs[:,:,:,3:6], [ni, ni], save_dir_ginit + '/_train_sample_96_2.png')
-    print('pass')
 
     """
     tl.vis.save_images(train_lr_vid_seqs[:,:,:,3:6], [ni, ni], save_dir_ginit + '/_train_sample_96_2.png')
@@ -218,7 +213,7 @@ def train():
     print(" ** fixed learning rate: %f (for init G)" % lr_init)
 
     train_vid_list = train_vid_list[0:5000] #5000
-    #train_lr_vid_list = train_lr_vid_list[0:12] #5000
+
     for epoch in range(0, n_epoch_init + 1):
 
         epoch_time = time.time()
@@ -413,6 +408,62 @@ def train():
             tl.files.save_npz(net_g.all_params, name=checkpoint_dir + '/g_{}.npz'.format(tl.global_flag['mode']), sess=sess)
             tl.files.save_npz(net_d.all_params, name=checkpoint_dir + '/d_{}.npz'.format(tl.global_flag['mode']), sess=sess)
 
+def evaluate():
+    ## create folders to save result images
+    save_dir = "samples/{}".format(tl.global_flag['mode'])
+    tl.files.exists_or_mkdir(save_dir)
+    checkpoint_dir = "checkpoint"
+
+    ###====================== PRE-LOAD DATA ===========================###
+    # train_hr_img_list = sorted(tl.files.load_file_list(path=config.TRAIN.hr_img_path, regx='.*.png', printable=False))
+    # train_lr_img_list = sorted(tl.files.load_file_list(path=config.TRAIN.lr_img_path, regx='.*.png', printable=False))
+    valid_hr_img_list = sorted(tl.files.load_file_list(path=config.VALID.hr_img_path, regx='.*.png', printable=False))
+    valid_lr_img_list = sorted(tl.files.load_file_list(path=config.VALID.lr_img_path, regx='.*.png', printable=False))
+
+    ## If your machine have enough memory, please pre-load the whole train set.
+    # train_hr_imgs = tl.vis.read_images(train_hr_img_list, path=config.TRAIN.hr_img_path, n_threads=32)
+    # for im in train_hr_imgs:
+    #     print(im.shape)
+    valid_lr_imgs = tl.vis.read_images(valid_lr_img_list, path=config.VALID.lr_img_path, n_threads=32)
+    # for im in valid_lr_imgs:
+    #     print(im.shape)
+    valid_hr_imgs = tl.vis.read_images(valid_hr_img_list, path=config.VALID.hr_img_path, n_threads=32)
+    # for im in valid_hr_imgs:
+    #     print(im.shape)
+    # exit()
+
+    ###========================== DEFINE MODEL ============================###
+    imid = 81  # 0: 企鹅  81: 蝴蝶 53: 鸟  64: 古堡  #64
+    valid_lr_img = valid_lr_imgs[imid]
+    valid_hr_img = valid_hr_imgs[imid]
+    # valid_lr_img = get_imgs_fn('test.png', 'data2017/')  # if you want to test your own image
+    valid_lr_img = (valid_lr_img / 127.5) - 1  # rescale to ［－1, 1]
+    # print(valid_lr_img.min(), valid_lr_img.max())
+
+    size = valid_lr_img.shape
+    # t_image = tf.placeholder('float32', [None, size[0], size[1], size[2]], name='input_image') # the old version of TL need to specify the image size
+    t_image = tf.placeholder('float32', [1, None, None, 3], name='input_image')
+
+    net_g = SRGAN_g(t_image, is_train=False, reuse=False)
+
+    ###========================== RESTORE G =============================###
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
+    tl.layers.initialize_global_variables(sess)
+    tl.files.load_and_assign_npz(sess=sess, name=checkpoint_dir + '/g_srgan.npz', network=net_g)
+
+    ###======================= EVALUATION =============================###
+    start_time = time.time()
+    out = sess.run(net_g.outputs, {t_image: [valid_lr_img]})
+    print("took: %4.4fs" % (time.time() - start_time))
+
+    print("LR size: %s /  generated HR size: %s" % (size, out.shape))  # LR size: (339, 510, 3) /  gen HR size: (1, 1356, 2040, 3)
+    print("[*] save images")
+    tl.vis.save_image(out[0], save_dir + '/valid_gen.png')
+    tl.vis.save_image(valid_lr_img, save_dir + '/valid_lr.png')
+    tl.vis.save_image(valid_hr_img, save_dir + '/valid_hr.png')
+
+    out_bicu = scipy.misc.imresize(valid_lr_img, [size[0] * 4, size[1] * 4], interp='bicubic', mode=None)
+    tl.vis.save_image(out_bicu, save_dir + '/valid_bicubic.png')
 
 
 if __name__ == '__main__':
