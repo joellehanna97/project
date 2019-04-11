09005257cb67269d9ddfe86436ca6aebfa712bdc
Joelle Hanna <joelle.hanna@epfl.ch>
2019-04-11 15:33:06 +0200

    qq

--- main.py
+++ main.py
@@ -593,6 +593,7 @@ def validate():
         #train_target_vid_imgs = tl.vis.read_images([train_vid_img_list[20],train_vid_img_list[50],train_vid_img_list[80],train_vid_img_list[110]], path=train_vid_list[0] + '/frames/', n_threads=32)
 
         train_vid_img_list_s1 = tl.vis.read_images(train_vid_img_list_s1,path=train_vid_list[i] + '/frames/', n_threads=32)
+        """
         target = tl.vis.read_images(train_vid_img_list[20],path=train_vid_list[i] + '/frames/', n_threads=32)
         print('shape is')
         print(np.shape(target))
@@ -600,6 +601,7 @@ def validate():
         print(type(target))
         target = tl.prepro.threading_data(target, fn = crop_sub_imgs_fn_2, is_random = False)
         tl.vis.save_image(target, save_dir + '/target_%d.png' %i)
+        """
 
         train_vid_img_list_s1 = tl.prepro.threading_data(train_vid_img_list_s1, fn = crop_sub_imgs_fn_2, is_random=False)
 
