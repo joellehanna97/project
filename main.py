d6d8774e4a99da636d4811423694ef7d41ea4f9a
Joelle Hanna <joelle.hanna@epfl.ch>
2019-05-09 15:54:51 +0200

    out

--- main.py
+++ main.py
@@ -499,8 +499,11 @@ def train():
             errD, _ = sess.run([d_loss, d_optim], {t_image: b_seqs_96, t_target_image: b_imgs_384})
             """
 
-            b_fake_3_value = sess.run(b_fake_3)
-            print(type(b_fake_3_value))
+            out = sess.run(net_g.outputs, {t_image: b_seqs_96})
+            print('out')
+            print(type(out))
+            #b_fake_3_value = sess.run(b_)
+            #print(type(b_fake_3_value))
             #b_fake_3 = b_fake_3.numpy()
             errD, _ = sess.run([d_loss, d_optim], {t_images_3: b_fake_3, t_target_images_3: b_seqs_384})
 
